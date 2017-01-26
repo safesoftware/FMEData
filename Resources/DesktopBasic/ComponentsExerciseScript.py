8bf3b8d6fdb5a8deaa9a39789635850c63b04525
FMEEvangelist <mark.ireland@safe.com>
2017-01-26 15:10:40 -0600

    2017 Chapter 4 Exercise 1

--- Resources/DesktopBasic/ComponentsExerciseScript.py
+++ Resources/DesktopBasic/ComponentsExerciseScript.py
@@ -3,4 +3,4 @@ import os
 
 if not os.path.exists('C:/FundraisingWalk'):
     os.makedirs('C:/FundraisingWalk')
-shutil.copy2('c:/FMEData2016/Output/Training/FundraisingWalk.kml', 'C:/FundraisingWalk/FundraisingWalk.kml')
+shutil.copy2('c:/FMEData2017/Output/Training/FundraisingWalk.kml', 'C:/FundraisingWalk/FundraisingWalk.kml')
